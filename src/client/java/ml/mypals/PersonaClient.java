d30b4d45e32abedf244f47724ccb6c8fac8787ba
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2026-01-21 08:57:05 +0900

    templet-init

---
+++ src/client/java/ml/mypals/PersonaClient.java
@@ -0,0 +1,10 @@
+package ml.mypals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PersonaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/ml/mypals/PersonaDataGenerator.java
@@ -0,0 +1,11 @@
+package ml.mypals;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PersonaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
